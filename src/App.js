b1abc9a6d429286c04c678bec9a0b1de1a218fcf
Brandon Ward <brandonward.gk@gmail.com>
2021-06-15 11:04:13 -0600

    add firebase to app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  // your config here
+  apiKey: "AIzaSyCX3AYP5tiMgCHDrxTrfIgdhRL_pRZAP48",
+  authDomain: "first-messaging-app-edb0b.firebaseapp.com",
+  projectId: "first-messaging-app-edb0b",
+  storageBucket: "first-messaging-app-edb0b.appspot.com",
+  messagingSenderId: "607507609857",
+  appId: "1:607507609857:web:c961b3b4f527eb055dc300",
+  measurementId: "G-WKCVCB8KN1"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
